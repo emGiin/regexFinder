def0a01526190d05b70f89c657cae795a84ffa29
emGiin <emGiin@users.noreply.github.com>
2018-06-19 22:16:07 +0100

    Update background.js

--- background.js
+++ background.js
@@ -2,5 +2,5 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.newIconPath) {
     chrome.browserAction.setIcon({ path: request.newIconPath });
     chrome.browserAction.setBadgeText({ text: '' });
-  } else chrome.browserAction.setBadgeText({ text: request.n });
+  } else if(request.n) chrome.browserAction.setBadgeText({ text: request.n });
 });
